945bf2a17f5e4cd4db95db347ace600d4053b977
Kai Gonzalez <gkai70264@gmail.com>
2022-04-22 17:04:21 -0400

    Documentation ??

--- Source.jl
+++ Source.jl
@@ -1,11 +1,13 @@
 module Source
 
+    # Function
     function func1()
         println("Hello World!")
     end
 
 end
 
+# load a library
 function LoadLib(lname)
     include("lib/" * lname * ".jl")
 end
