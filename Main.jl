301fade701a1a4078731ab13e9594d851849d93c
Kai Gonzalez <gkai70264@gmail.com>
2022-04-22 16:46:07 -0400

    first commit

---
+++ Main.jl
@@ -0,0 +1,8 @@
+# Create and strip
+idea = strip("This idea");
+
+idea == "This idea" ? println("I have ideas!") : println("I don't have ideas")
+
+include("Source.jl")
+
+Source.func1()
---
+++ Source.jl
@@ -0,0 +1,7 @@
+module Source
+
+    function func1()
+        println("Hello World!")
+    end
+
+end
